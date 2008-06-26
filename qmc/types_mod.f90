3f3762771f9fb0fc3675273973be1e98668e8525
Cyrus Umrigar <CyrusUmrigar@cornell.edu>
2008-06-26 02:00:31 +0000

    Richards change to Makefiles for libs for Jaguar. Also, main Makefile for lib so that one can make all the libraries by just typing make in lib
    git-svn-id: svn://hydrogene.lct.jussieu.fr/champ@131 36b7402c-5635-0410-8f0e-a47a0fc0da44

--- qmc/types_mod.f90
+++ qmc/types_mod.f90
@@ -1,9 +1,9 @@
 module types_mod
 
-!
-!  integer, parameter :: I4B = SELECTED_INT_KIND(9)
-!  integer, parameter :: I2B = SELECTED_INT_KIND(4)
-!  integer, parameter :: I1B = SELECTED_INT_KIND(2)
+!  integer, parameter :: i8b = SELECTED_INT_KIND(18)
+!  integer, parameter :: i4b = SELECTED_INT_KIND(9)
+!  integer, parameter :: i2b = SELECTED_INT_KIND(4)
+!  integer, parameter :: i1b = SELECTED_INT_KIND(2)
 !  integer, parameter :: LGT = KIND(.true.)
 
   integer, parameter :: dp = kind(1.0d0)
