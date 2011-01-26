5f64a5d6aa8295eeff10e6e646c486b5ceb57fc0
Abhijit Mehta <abhijit.mehta@gmail.com>
2011-01-26 00:17:31 +0000

    Added variables to optimo_mod to control constrained optimization
    git-svn-id: svn://hydrogene.lct.jussieu.fr/champ@494 36b7402c-5635-0410-8f0e-a47a0fc0da44

--- qmc/commons/optimo_mod.f90
+++ qmc/commons/optimo_mod.f90
@@ -4,7 +4,9 @@ module optimo_mod
  implicit none
  save
 
- integer nparmot,notype
+ integer nparmot,notype,iconstrain_gauss_orbs
  integer, allocatable :: iwo(:,:), nparmo(:)
+ integer, allocatable :: norb_constraints(:), orb_constraints(:,:,:)
+
 
 end module optimo_mod
