66d7972f3008325b749ed7196e5e7f52566c6c53
Abhijit Mehta <abhijit.mehta@gmail.com>
2011-10-11 21:24:02 +0000

    small changes to fix bug when ndn =0 with mpi function calls.
    git-svn-id: svn://hydrogene.lct.jussieu.fr/champ@566 36b7402c-5635-0410-8f0e-a47a0fc0da44

--- qmc/dmc/dmc_elec/MPI_global_pop_big/walksav_det_mpi.f
+++ qmc/dmc/dmc_elec/MPI_global_pop_big/walksav_det_mpi.f
@@ -26,11 +26,11 @@ c Only the entries are called, not the subroutine
         call MPI_Wait(irequest,istatus,ierr)
         call mpi_isend(fpuw(1,1,k,nwalk),3*nup*nup,mpi_double_precision,irecv,itag+2,MPI_COMM_WORLD,irequest,ierr)
         call MPI_Wait(irequest,istatus,ierr)
-        call mpi_isend(slmdiw(1,k,nwalk),ndn*ndn,mpi_double_precision,irecv,itag+3,MPI_COMM_WORLD,irequest,ierr)
+        call mpi_isend(slmdiw(:,k,nwalk),ndn*ndn,mpi_double_precision,irecv,itag+3,MPI_COMM_WORLD,irequest,ierr)
         call MPI_Wait(irequest,istatus,ierr)
-        call mpi_isend(fpdw(1,1,k,nwalk),3*ndn*ndn,mpi_double_precision,irecv,itag+4,MPI_COMM_WORLD,irequest,ierr)
+        call mpi_isend(fpdw(:,:,k,nwalk),3*ndn*ndn,mpi_double_precision,irecv,itag+4,MPI_COMM_WORLD,irequest,ierr)
         call MPI_Wait(irequest,istatus,ierr)
-        call mpi_isend(ddeti_detiw(1,1,k,nwalk),3*nelec,mpi_double_precision,irecv,itag+5,MPI_COMM_WORLD,irequest,ierr)
+        call mpi_isend(ddeti_detiw(:,:,k,nwalk),3*nelec,mpi_double_precision,irecv,itag+5,MPI_COMM_WORLD,irequest,ierr)
         call MPI_Wait(irequest,istatus,ierr)
   150   itag=itag+5
       return
@@ -44,9 +44,9 @@ c Only the entries are called, not the subroutine
       do 160 k=1,ndet
         call mpi_recv(slmuiw(1,k,nwalk),nup*nup,mpi_double_precision,isend,itag+1,MPI_COMM_WORLD,istatus,ierr)
         call mpi_recv(fpuw(1,1,k,nwalk),3*nup*nup,mpi_double_precision,isend,itag+2,MPI_COMM_WORLD,istatus,ierr)
-        call mpi_recv(slmdiw(1,k,nwalk),ndn*ndn,mpi_double_precision,isend,itag+3,MPI_COMM_WORLD,istatus,ierr)
-        call mpi_recv(fpdw(1,1,k,nwalk),3*ndn*ndn,mpi_double_precision,isend,itag+4,MPI_COMM_WORLD,istatus,ierr)
-        call mpi_recv(ddeti_detiw(1,1,k,nwalk),3*nelec,mpi_double_precision,isend,itag+5,MPI_COMM_WORLD,istatus,ierr)
+        call mpi_recv(slmdiw(:,k,nwalk),ndn*ndn,mpi_double_precision,isend,itag+3,MPI_COMM_WORLD,istatus,ierr)
+        call mpi_recv(fpdw(:,:,k,nwalk),3*ndn*ndn,mpi_double_precision,isend,itag+4,MPI_COMM_WORLD,istatus,ierr)
+        call mpi_recv(ddeti_detiw(:,:,k,nwalk),3*nelec,mpi_double_precision,isend,itag+5,MPI_COMM_WORLD,istatus,ierr)
   160   itag=itag+5
 
 # endif
